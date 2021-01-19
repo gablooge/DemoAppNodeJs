41259b3ac3d8a458365dcbf7057dec88b52a8140
Samsul Hadi <samsulhadi@Samsuls-MacBook-Air.local>
2021-01-19 10:36:43 +0700

    /messages

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 
+var messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jane', message: 'Hello'}
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
+
 app.use(express.static(__dirname));
 var server = app.listen(3000, () => {
     console.log('server is listening on port 3000', server.address().port)
